76561853b09ae45032c406e915f7f150fe629ec6
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-06-30 20:57:49 -0700

    change the layout for navbar

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} grainy`}>
+        <div className="h-32 w-screen">
+          <Navbar/>
+        </div>
+        <div className="w-screen h-[calc(100vh-128px)] overflow-x-hidden">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,6 @@ import Navbar from "./components/Navbar";
 export default function Home() {
   return (
     <div className="min-h-screen w-screen grainy overflow-x-hidden overflow-y-auto">
-      <Navbar/>
       <div className="h-full w-full flex flex-col mt-14 md:px-32 px-12">
         <h1 className="text-2xl mb-10">BIOMEDICAL ENGINEERING</h1>
         <div className="flex flex-col md:flex-row w-full mt-10 gap-5">
