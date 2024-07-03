54632a269f71a578d062cf4c1a08234a3b8ae33a
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-07-03 16:00:08 -0700

    adding the stairAnimation

--- app/components/Stairs.jsx
+++ app/components/Stairs.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 
 const Stairs = () => {
+
+    const stairAnimation = {
+        initial: {
+            top: "0%"
+        },
+        animate: {
+            top: "100%"
+        },
+        exit: {
+            top: ["100%", "0%"]
+        }
+    }
   return (
-    <div>
-      Stairs
-    </div>
+    <>
+            
+    </>
   )
 }
 
