893141c7ad1f5fc40d598102a2d3cb41e485d811
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-07-08 14:18:22 -0700

    fixed the sizing in the about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -4,11 +4,14 @@ import React from 'react'
 const page = () => {
   return (
     <div className='h-full w-full md:px-32 px-12'>
-      <div className='h-full mt-20 flex gap-8'>
-        <div className=' border-gray-800 border-2 rounded-xl p-3'>
-          <img src={"/flag.jpg"} alt='flag'/>
+      <div className='h-full mt-20 flex md:flex-row justify-center flex-col gap-8 pb-10'>
+        {/* <div className=' border-gray-800 border-2 rounded-xl p-3'>
+          <img className='border' src={"/flag.jpg"} alt='flag'/>
+        </div> */}
+        <div>
+          <img className='border-gray-800 border-2 rounded-2xl p-3 w-[450px]' src={"/flag.jpg"} alt='flag'/>
         </div>
-        <div className=' w-[60%] flex flex-col'>
+        <div className='md:w-[50%]'>
           <h3 className='font-semibold text-xl mb-6'>About Me</h3>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam inventore, similique necessitatibus blanditiis nobis fuga voluptatum quam molestias consectetur repudiandae enim eaque soluta itaque quasi, tempore expedita. Exercitationem, fugiat doloribus.</p>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, quasi autem dolorem delectus modi omnis fugiat numquam voluptates? Quasi vitae blanditiis debitis voluptatum ab odit optio obcaecati tempora quo! Accusantium.</p>
--- app/page.jsx
+++ app/page.jsx
@@ -4,13 +4,13 @@ import Navbar from "./components/Navbar";
 export default function Home() {
   return (
     <div className="h-full w-full grainy overflow-x-hidden overflow-y-auto md:px-32 px-12">
-      <div className="h-full w-full flex flex-col justify-center items-center mt-14">
+      <div className="h-full w-full flex flex-col justify-center items-center mt-14 pb-10">
         <h1 className="text-2xl mb-10">BIOMEDICAL ENGINEERING</h1>
-        <div className="flex justify-center mt-10 gap-4">
+        <div className="flex md:flex-row flex-col justify-center mt-10 gap-4">
           <div>
             <img src={"/home.png"} className="rounded-2xl"/>
           </div>
-          <div className="w-[50%]">
+          <div className="md:w-[50%]">
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam, quo voluptatum est, perspiciatis quaerat eos magni velit dicta nesciunt nihil, exercitationem illum deleniti sequi. Sapiente neque itaque consequuntur ipsa illo.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatum in quisquam porro illo magni consequatur cupiditate, aliquam nam molestiae ratione mollitia modi cum. Omnis, explicabo. Odit inventore minus quod.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe consectetur voluptatum consequuntur accusantium! Amet quia iusto sint sit unde minima, omnis maxime itaque quae impedit, quisquam beatae, obcaecati cum aperiam?</p>
