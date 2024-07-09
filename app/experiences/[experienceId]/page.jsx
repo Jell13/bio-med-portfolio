5c6876d09f9897d1145e1ca5c7cea2584f8ee624
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-07-09 14:14:24 -0700

    making the carousel

---
+++ app/experiences/[experienceId]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Testing
+    </div>
+  )
+}
+
+export default page
--- app/experiences/page.jsx
+++ app/experiences/page.jsx
@@ -7,26 +7,29 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel"
+import Link from "next/link"
 
 const page = () => {
   return (
     <div className='w-full h-full md:px-32 px-12'>
-      <div className='h-full flex flex-col mt-20'>
+      <div className='h-full flex flex-col mt-20 pb-10'>
         <h1 className='text-3xl font-medium'>Experiences</h1>
         <div className='flex justify-center items-center flex-col'>
             {/* <div className='mt-8'>
               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit iusto fuga veniam nostrum corrupti, deserunt expedita ratione, dicta illo consectetur reiciendis ipsum deleniti alias minus quos porro amet saepe. Sint.</p>
             </div> */}
-            <Carousel>
+            <Carousel className="mt-20">
               <CarouselContent>
               {Array.from({ length: 5 }).map((_, index) => (
                 <CarouselItem key={index}>
                   <div className="p-1">
-                    <Card>
-                      <CardContent className="flex aspect-square items-center justify-center p-6">
-                        <span className="text-4xl font-semibold">{index + 1}</span>
-                      </CardContent>
-                    </Card>
+                    <Link href={`/experiences/${index}`}>
+                      <Card>
+                        <CardContent className="flex aspect-square items-center justify-center p-6">
+                          <span className="text-4xl font-semibold">{index + 1}</span>
+                        </CardContent>
+                      </Card>
+                    </Link>
                   </div>
                 </CarouselItem>
               ))}
