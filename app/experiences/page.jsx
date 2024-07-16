f853ab994e74037434d0cde4d63454bf0e3f2932
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-07-15 21:53:03 -0700

    added the lorem

--- app/experiences/page.jsx
+++ app/experiences/page.jsx
@@ -15,10 +15,9 @@ const page = () => {
       <div className='h-full flex flex-col mt-10 pb-10'>
         <h1 className='text-3xl font-medium'>Experiences</h1>
         <div className='flex justify-center items-center flex-col'>
-            {/* <div className='mt-8'>
+            <div className='mt-8'>
               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit iusto fuga veniam nostrum corrupti, deserunt expedita ratione, dicta illo consectetur reiciendis ipsum deleniti alias minus quos porro amet saepe. Sint.</p>
-            </div> */}
-            
+            </div>
         </div>
       </div>
     </div>
